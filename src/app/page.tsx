0d37b723b69df324414c88f6bac86d9079abd4cb
anmoltiwari0225 <anmoltiwari0225@gmail.com>
2024-05-30 01:08:31 +0530

    created nav storybook component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,4 @@
-"use client"
-import { Button } from "@/components/Button";
-import { signIn, useSession } from "next-auth/react";
-
 export default function Home() {
-  const {data: session, status} = useSession();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     </main>
